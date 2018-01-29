b1f3d37476db783db4e17899e78dd1c4a248c806
dkujnisc <dkujnisc@smail.uni-koeln.de>
2018-01-29 22:42:07 +0100

    Maps und Listen
    Der Parser soll die Daten aus der XML-Datei in Maps und Listen überführen. Anfängliches Grundgerüst wurde erstellt. Debugging Probleme

--- seminararbeit_kujnisch/main.cpp
+++ seminararbeit_kujnisch/main.cpp
@@ -1,11 +1,41 @@
 #include <iostream>
 #include <fstream>
+#include <map>
+#include <list>
 
 using namespace std;
+
+//list< map<string,string> > listOfMaps;
+map<string,string> readGame(){
+    map<string,string> result;
+    result.insert(pair<string,string>('gamename','Super Mario Land'));
+    result.insert(pair<string,string>('gamenamy','Super Mario Land'));
+    result.insert(pair<string,string>('gamenamk','Super Mario Land'));
+    result.insert(pair<string,string>('gamenamo','Super Mario Land'));
+    result.insert(pair<string,string>('gamenamp','Super Mario Land'));
+    result.insert(pair<string,string>('gamenamt','Super Mario Land'));
+    result.insert(pair<string,string>('gamenami','Super Mario Land'));
+    return result;
+}
+void addGame(map<string,string> gameMap){
+    listOfMaps[0]=gameMap;
+}
+
+
 
 int main()
 {
     ifstream eingabe;
+    list< map<string,string> > listOfMaps;
+
+    listOfMaps[0]=readGame();
+
+     //map<string, map<string,string>>::iterator i = m.find("hello");
+      //assert(i != m.end());
+    for (list<map<string,string>>::iterator it = listOfMaps.begin(); it != listOfMaps.end(); it++)
+        cout << *it << 'xxx';
+      //std::cout << "Key: " << i->first << " Value: " << i->second << '\n';
+
 
     char zeichen;
     //int i;
@@ -20,6 +50,6 @@ int main()
         eingabe.get(zeichen);
     }
 
-    cout << "Hello World!" << endl;
+    cout << "Hello Worlk!" << endl;
     return 0;
 }
