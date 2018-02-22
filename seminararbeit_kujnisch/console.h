3193766ca85d9b1b9a936994c03fb98c582e42d5
dkujnisc <dkujnisc@smail.uni-koeln.de>
2018-02-22 21:37:14 +0100

    Klassen
    Einfügen der Klassen entsprechend XML-Datenquelle

--- seminararbeit_kujnisch/console.h
+++ seminararbeit_kujnisch/console.h
@@ -5,9 +5,15 @@
 class Parser
 {
     public:
+
+        string console_name;
+        string console_publisher;
+        string console_year;
+        string console_type;
+
         char zeichen;
-        int zaehler;
-        int zustand;
+        int zaehler=0;
+        int zustand=0;
         /* dabei gilt:
            zustand = 0 : standardzustand
            zustand = 1 : sonderfall familienname
@@ -18,3 +24,10 @@ class Parser
 } ;
 
 #endif
+
+<console>
+    <console_name>Gameboy</console_name>
+    <console_publisher>Nintendo</console_publisher>
+    <console_year>1989</console_year>
+    <console_type>Handheld-Konsole</console_type>
+</console>
