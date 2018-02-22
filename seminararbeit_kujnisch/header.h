8f8bee7828db522bfdb312813eb3d5aa2119bc54
dkujnisc <dkujnisc@smail.uni-koeln.de>
2018-02-22 23:02:22 +0100

    Neuer Header
    neuer Header nach Umbenennung von Console.h in header.n

---
+++ seminararbeit_kujnisch/header.h
@@ -0,0 +1,33 @@
+#ifndef CONSOLE_H
+#define CONSOLE_H
+
+using namespace std;
+
+class Parser
+{
+    public:
+
+        string console_name;
+        string console_publisher;
+        string console_year;
+        string console_type;
+
+        bool start=true;
+
+        int krokodilcounter=0;
+
+        char zeichen;
+        int zaehler=0;
+        int element=-1;
+        /* dabei gilt:
+           element = 0 : console_name
+           element = 1 : console_publisher
+           element = 2 : console_year
+           element = 3 : console_type
+        */
+        int parsezeichen();
+    private:
+        char puffer[100];
+} ;
+
+#endif
