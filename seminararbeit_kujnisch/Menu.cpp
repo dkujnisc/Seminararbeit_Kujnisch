ca56944872c591ddbbad3d2de1d4008b11af4632
dkujnisc <dkujnisc@smail.uni-koeln.de>
2018-03-26 21:15:49 +0200

    Export & Console Suche
    Vorbereitung für Export: Datei samt Inhalten schreiben und speichern an festgelegtem Ort
    include fstream für die Datei; include chrono für timestamp innerhalb Dateibenennung
    Erweiterung des Menüs um beenden
    Überarbeitung des Hilfe-Dialogs
    Suche nach Konsolen ist jetzt auch möglich

--- seminararbeit_kujnisch/Menu.cpp
+++ seminararbeit_kujnisch/Menu.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <sstream>
 #include <cstring>
+#include <fstream>
+#include <chrono>
 #include "header.h"
 
 using namespace std;
@@ -20,6 +22,9 @@ void Menu::startMenu(Console consoleList[], Game gameList[]){
     // hilfe
     string menuEingabe_h= "h";
     string menuEingabe_hilfe= "hilfe";
+    // beenden
+    string menuEingabe_b= "b";
+    string menuEingabe_beenden= "beenden";
 
      while (true) {
        getline(cin, input);
@@ -28,15 +33,46 @@ void Menu::startMenu(Console consoleList[], Game gameList[]){
            // suche
            if (0==strcmp(menuEingabe_s.c_str(), input.c_str()) || 0==strcmp(menuEingabe_suche.c_str(), input.c_str())) {
                suche(consoleList, gameList);
-               cout << "Mist, wir sind zurueck im hauptmenu" << endl;
+               startMenuAusgabe();
            }
            // export
            if (0==strcmp(menuEingabe_e.c_str(), input.c_str()) || 0==strcmp(menuEingabe_export.c_str(), input.c_str())) {
-               break;
+                 //cout << "Gib einen Exportdateinamen ein!" << endl;
+                 //string dateiName = "";
+                 //getline(cin, dateiName);
+                 //cout << "Deine Datei wird unter folgendem Pfad gespeichert: //Users//denise//Dokumente//" + dateiName + ".xml" << endl;
+
+               // quelle: https://stackoverflow.com/questions/19555121/how-to-get-current-timestamp-in-milliseconds-since-1970-just-the-way-java-gets
+               // timestamp für exportdateien
+               std::chrono::milliseconds ms = std::chrono::duration_cast< std::chrono::milliseconds >(
+                    std::chrono::system_clock::now().time_since_epoch());
+               ofstream filestr;
+               //filestr.open ("//Users//denise//Dokumente//export" + std::to_string(ms.count()) + ".xml");
+
+               // quelle: https://stackoverflow.com/questions/31860405/how-to-concatenate-a-string-with-chronomilliseconds
+               // unwandlung ms in string
+               filestr.open ("//Users//denise//Documents//export" + std::to_string(ms.count()) + ".xml");
+               std::streambuf *coutbuf = std::cout.rdbuf();
+               cout.rdbuf(filestr.rdbuf());
+               cout << "Wir sind einfach die Aller Aller Aller GEILSTEN!";
+               // wiederherstellung des alten coutbuf
+               std::cout.rdbuf(coutbuf);
+               // zum abschliessen der datei; aehnlich wie speichern
+               filestr.close();
+               cout << "Export war erfolgreich" << endl;
+               cout << "Folgende Datei wurde geschrieben://Users//denise//Documents//export" + std::to_string(ms.count()) + ".xml" << endl;
+               startMenuAusgabe();
            }
            // hilfe
            if (0==strcmp(menuEingabe_h.c_str(), input.c_str()) || 0==strcmp(menuEingabe_hilfe.c_str(), input.c_str())) {
+               cout << "Gib den in Klammern stehenden Buchstaben an oder das Wort." << endl;
+               cout << "Beispiel:(b)eenden" << endl;
+               cout << "Hier musst Du b oder beenden eingeben." << endl;
+           }
+           // beenden
+           if (0==strcmp(menuEingabe_b.c_str(), input.c_str()) || 0==strcmp(menuEingabe_beenden.c_str(), input.c_str())) {
                break;
+
            }
      }
     }
@@ -47,6 +83,7 @@ void Menu::suche(Console consoleList[], Game gameList[]){
     // suche
     string inputSuche = "";
     string gameSuche = "";
+    string consoleSuche = "";
     // game
     string menuEingabe_g= "g";
     string menuEingabe_game= "game";
@@ -82,7 +119,20 @@ void Menu::suche(Console consoleList[], Game gameList[]){
           }
           // console
           if (0==strcmp(menuEingabe_c.c_str(), inputSuche.c_str()) || 0==strcmp(menuEingabe_console.c_str(), inputSuche.c_str())) {
-              break;
+              cout << "Gib einen Suchnamen ein! Achtung: case sensitive!" << endl;
+              getline(cin, consoleSuche);
+              cout << "nach folgendem Begriff wird gesucht: \'" + consoleSuche + "\'" << endl;
+              bool nixGefunden=true;
+              for(unsigned i = 1; i <= (sizeof(Console)/sizeof(consoleList)); i++) {
+                   if (std::string::npos!=consoleList[i].console_name.find(consoleSuche)) {
+                          cout << consoleList[i].console_name << endl;
+                        nixGefunden=false;
+                   }
+              }
+              if (nixGefunden) {
+                  cout << "Leider nix gefunden." << endl;
+              }
+              suchMenuAusgabe();
           }
           // zurueck
           if (0==strcmp(menuEingabe_z.c_str(), inputSuche.c_str()) || 0==strcmp(menuEingabe_zurueck.c_str(), inputSuche.c_str())) {
@@ -90,7 +140,9 @@ void Menu::suche(Console consoleList[], Game gameList[]){
           }
           // hilfe
           if (0==strcmp(menuEingabe_h.c_str(), inputSuche.c_str()) || 0==strcmp(menuEingabe_hilfe.c_str(), inputSuche.c_str())) {
-              break;
+              cout << "Gib den in Klammern stehenden Buchstaben an oder das Wort." << endl;
+              cout << "Beispiel:(g)ames suchen" << endl;
+              cout << "Hier musst Du g oder games eingeben." << endl;
           }
     }
     }
@@ -110,4 +162,5 @@ void Menu::startMenuAusgabe(){
     cout << "(s)uche" << endl;
     cout << "(e)xport" << endl;
     cout << "(h)ilfe" << endl;
+    cout << "(b)eenden" << endl;
 }
